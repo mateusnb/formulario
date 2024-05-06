c9529606dabfc809da1d340e5fdf56d2538972c3
Mateus Baptista <mateusnb@gmail.com>
2024-05-06 18:23:50 -0300

    Update email-form.js

--- Contact-Form/assets/email-form.js
+++ Contact-Form/assets/email-form.js
@@ -7,7 +7,7 @@ document.getElementById("gform").addEventListener('submit', (e) => {
  subscribeForm.setAttribute("style", "-webkit-animation: fadeOut 1s; animation: fadeOut 1s;  animation-fill-mode: forwards;");
  document.getElementById("gform").setAttribute("style", "display:none;");  
  
-subscribeForm.innerHTML = `<h1>Your Email Has Sent!</h1> `   
+subscribeForm.innerHTML = `<h1>Your Email Has Enviado!!!!!!</h1> `   
   
 subscribeForm.setAttribute("style", "-webkit-animation: fadeIn 1s; animation: fadeIn 1s;  animation-fill-mode: forwards;");  
 
